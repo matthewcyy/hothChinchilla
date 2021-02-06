f54905bb63a9bea17f68f3a94474a69c6b880142
Adrian Lam <adrianlam@g.ucla.edu>
2021-02-06 08:48:48 +0000

    Create main.py
    Main framework file

---
+++ main.py
@@ -0,0 +1,25 @@
+#import libraries
+import pygame
+#pygame initialization
+pygame.init()
+
+#colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+#framework
+size = (750, 1334)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Chinchilla")
+
+#~~~~ Game loop initialization ~~~~
+gameRunning = True
+
+#------------- Main program ---------------
+while gameRunning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameRunning = False
